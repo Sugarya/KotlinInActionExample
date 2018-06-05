f6fcabfadeae26b3e3c70fa4823ae8b20a229ebf
EthanRuan <ruanchenhui@ZBMAC-C02VQ2ABH.local>
2018-06-05 11:44:57 +0800

    update 6th chapter

--- src/sugarya/chapter6/Example6.kt
+++ src/sugarya/chapter6/Example6.kt
@@ -1,16 +1,21 @@
 package sugarya.chapter6
 
 fun main(args: Array<String>) {
-    testNoNullClient()
+//    testNoNullClient()
+//    testAnyAndUnit()
+//    testNullType()
+    testAsClient()
 }
 
 
 //Kotlin基本类型和转化
 
-fun testBasicType() {
+fun testBasicType(){
+
+    //Kotlin层面上不区分基本数据类型和包装类型，转成Java时有不同
     val bb: Byte = 127
     val boolean = false
-    val aa: Char = 'a'
+    val aa: Char = '+'
 
     var i: Int = 21_4748_3647 //10位
     var l: Long = 922_3372_0368_5477_5807L  //19位 //不存在小写的l
@@ -19,6 +24,8 @@ fun testBasicType() {
     val f: Float = 123f
     val f2: Float = 124F
 
+    val d: Double = 12.5
+
     val a16 = 0x123ef3
     val b16 = 0XcCCef3
 
@@ -26,28 +33,44 @@ fun testBasicType() {
     val cc2 = 0B11001
 
     val b: Byte = 1
+    val eas = b + 1L //Long + Byte => Long , eas为Long类型;
 
-    val eas = b + 1L //Long + Byte => Long , eas为Long类型
+    //小范围不能自动转成大范围数据
+    val index: Int = 11
+//    val dd: Double = index
 
 
     fun foo(l: Long) {
         println(l)
     }
-
     foo(44) //单独写44可能是byte，int long，这里智能推断44会被认为是long
 
     //字符串转基本数据类型
     val p = "44".toInt()
     val p2 = "44".toLong()
     val p3 = "44".toFloatOrNull()
-    val p4 = "44".toDouble()
+
+    //Kotlin的异常处理，1. 不存在方法上throws  2.不区分受检异常和运行时异常，统统按运行时异常处理
+    try {
+        val p4 = "44".toDouble()
+    }catch (e: NumberFormatException){
+        e.printStackTrace()
+    }
 
     val translate = l.toInt().toLong().toFloat().toDouble().toInt()
 }
 
 
-//Any Unit Nothing
+//Any Unit
 
+fun testAnyAndUnit(){
+    val any: Any = testBasicType()
+
+    val a: Unit = testBasicType()
+    println("hashcode = ${a.hashCode()}, toString = ${a.toString()}")
+}
+
+// Nothing
 fun getStrinLenght(s: String?): Int {
     if (s == null) {
         throw IllegalArgumentException()
@@ -81,7 +104,8 @@ fun testAnyAndNothing(o: Any?): View {
     }
 }
 
-//空安全调用
+////////////空安全调用 ?.
+
 open class Company(val name: String, val address: String?)
 
 fun testNullType() {
@@ -91,19 +115,21 @@ fun testNullType() {
     println("length = $length")
 }
 
-//Elvis运算法
-fun testElvis(list: List<Company?>?, defaultAddress: String?) {
-    val defaultAdd = defaultAddress ?: ""
-    val length = defaultAddress?.length ?: 0 //.?为空调用，表达式返回null，遇到了？：
-    println("defaultAdd = $defaultAdd, length = $length")
+//Elvis运算法 ?:
+fun testElivis(){
+    val company = Company("京东", null)
 
-    val companyList = list ?: throw IllegalArgumentException("null list")
-
-    val address = companyList[0]?.address?.toUpperCase() ?: defaultAddress //多重空安全调用和Elvis联合使用
-    println("address = $address")
+    val length = company.address?.length?:0
+    println("length = $length")
 }
 
-fun getStrinLenght3(s: String?): Int = s?.length ?: fail()
+fun testElvis2(list: List<Company?>?, defaultAddress: String?) {
+    val companyList = list ?: throw IllegalArgumentException("null list")
+
+    val defaultAdd = defaultAddress ?: ""
+    val address = companyList[0]?.address?.toUpperCase() ?: defaultAdd //多重空安全调用和Elvis联合使用
+    println("address = $address")
+}
 
 //安全转换
 open class View(val name: String)
@@ -131,18 +157,32 @@ fun testAs2(o: Any) {
 
 
 //非空断言
-fun testNoNullClient() {
-    val result = testNoNull("Kotlin")
-    println("result = $result")
-}
-
 fun testNoNull(s: String?): String {
     val length = s!!.length
     println("$s's length is $length")
     return s!!.toUpperCase()
 }
 
-//let函数
+fun testNoNullClient() {
+    val result = testNoNull("Kotlin")
+    println("result = $result")
+}
+
+//对 List<Company> 惰性初始化
+class BookManage() {
+    private var _companyList: List<Company>? = null
+
+    val companyList: List<Company>
+        get() {
+            if (_companyList == null) {
+                _companyList = arrayListOf(Company("JD", "Beijing"))
+            }
+            return _companyList!!
+        }
+}
+
+//let函数  把可空类型 转成 非空类型
+fun getTheBigCompanyInTheWorld(): Company = Company("JD", "Beijing")
 
 fun testLet() {
     val length = getTheBigCompanyInTheWorld().address?.length
@@ -160,8 +200,6 @@ fun testLet2() {
     }
 }
 
-fun getTheBigCompanyInTheWorld(): Company = Company("JD", "Beijing")
-
 
 //可空类型的扩展函数
 
@@ -169,14 +207,19 @@ fun CharSequence?.isNullOrEmpty(): Boolean {
     return this == null || this.length == 0
 }
 
-fun testNullExtend() {
-    val company = Company("JD", null)
+fun CharSequence?.theLength(): Int =
+        if(this == null){
+            0
+        }else{
+            this.length
+        }
 
-    val nullOrEmpty = company.address.isNullOrEmpty()
+fun testNullExtend() {
+    val length = getTheBigCompanyInTheWorld().address.theLength()
+    println("length = $length")
 }
 
-//泛型参数是可空类型
-
+//特殊情况：泛型参数是可空类型
 fun testGenericityClient() {
     myPrintln(null)
 
--- src/sugarya/contrast/chapter4/Client.java
+++ src/sugarya/contrast/chapter4/Client.java
@@ -7,5 +7,10 @@ public class Client {
 
     public static void main(String[] args) {
         int wheel = MyUtils.wheel;
+
+        double d = 4d;
+        int a = 123;
+
     }
+
 }
